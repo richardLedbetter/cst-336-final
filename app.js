a8131731fead6e45d77ac5247809d87cc67d56e2
EC2 Default User <ec2-user@ip-172-31-35-235.us-east-2.compute.internal>
2019-12-03 21:52:03 +0000

    v1.2 of register function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 //var mysqlAdmin = require('node-mysql-admin');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var indexRouter = require('./routes/router');
@@ -27,6 +28,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
 
 app.use('/', indexRouter);
 
