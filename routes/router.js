584a0b3fad0f330603235d0bc0f891d6fda2563e
jakefahy <ec2-user@ip-172-31-29-254.ec2.internal>
2019-12-13 22:07:58 +0000

    trying to fix heroku v1

--- routes/router.js
+++ routes/router.js
@@ -1481,8 +1481,6 @@ function getImages(keyword){
     
 }
 
-/*router.listen(process.env.PORT, process.env.IP, function(){
-console.log("Express server is running...");
-});*/
+
 
 module.exports = router;
